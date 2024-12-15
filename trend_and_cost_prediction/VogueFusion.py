b3660577b5184316e9e4c3e051ead116ebd3d0a2
Aagam <aagamchhajer@gmail.com>
2024-12-15 13:25:00 +0530

    Added Web Assets

--- trend_and_cost_prediction/VogueFusion.py
+++ trend_and_cost_prediction/VogueFusion.py
@@ -6,5 +6,5 @@ st.set_page_config(
     layout="wide"
 )
 
-st.title("Welcome to the Saree Design App")
+st.title("Welcome to the Vogue Fusion App")
 st.write("Navigate to different sections using the sidebar.")
